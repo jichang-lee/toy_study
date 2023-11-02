8079520b144298ddff9df8c7495858d70df83e17
dlwlckdWkd12 <jccc23@naver.com>
2023-11-02 21:46:27 +0900

    webconfig_update

---
+++ testspring/frontend/src/views/WriterView.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <input 
+        v-model="title"
+        placeholder="제목을 입력해주세요"/>
+    </div>
+
+    <div>
+        <textarea 
+        v-model="content"
+         
+        rows="15"/>
+    </div>
+
+    <div >
+        <button 
+        @click="write"
+        > 글 작성완료</button>
+    </div>
+    
+
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import axios from 'axios';
+
+
+const title = ref("")
+const content = ref("")
+
+
+
+const write = ()=>{
+    axios.post("http://localhost:8099/posts",{
+     title: title.value,
+     content : content.value   
+    })
+}
+</script>
+
+<style>
+
+</style>
